60e87191f6ffa1f96927d46d3899942ca1b50511
Hemant Jha <156426952+DevHemantJha@users.noreply.github.com>
2024-04-25 11:25:51 +0530

    Fixed the upload yml structure

--- controllers/themeSettingController.js
+++ controllers/themeSettingController.js
@@ -20,13 +20,18 @@ const getRouteTheme = async (req, res, next) => {
 //       res.status(400).json({ message: 'Something wrong' });
 //     }
 //   };
-  const addRouteThemeController = async (req, res, next) => {
-    try {
-    console.log("addRouteThemeData",req.body);
+const addRouteThemeController = async (req, res, next) => {
+  try {
+    console.log("addRouteThemeData", req.body);
+    if (req.body != null && Object.keys(req.body).length !== 0) {
       const user = await addRouteThemeData(req.body);
       res.status(200).json({ message: 'Added Successfully' });
-    } catch (err) {
-      res.status(400).json({ message: 'Something wrong' });
+    } else {
+      res.status(400).json({ message: 'Request body is empty' });
     }
-  };
-module.exports = {addRouteThemeController,getRouteTheme};
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+module.exports = { addRouteThemeController, getRouteTheme };
